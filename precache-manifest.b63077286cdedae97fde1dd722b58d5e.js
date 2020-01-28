89eff12c09bf9733ad37055ee6ba487bb3462788
ubermachine <shivamkatoch@outlook.com>
2020-01-28 13:14:54 +0530

    Updates

---
+++ precache-manifest.b63077286cdedae97fde1dd722b58d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac313b064208b50c203c0ccba595f7a",
+    "url": "/react-randomQuoteGen/index.html"
+  },
+  {
+    "revision": "4504ff8af6c48062024f",
+    "url": "/react-randomQuoteGen/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d2185ea97b74b3bff5d5",
+    "url": "/react-randomQuoteGen/static/css/main.d0a9e5c3.chunk.css"
+  },
+  {
+    "revision": "4504ff8af6c48062024f",
+    "url": "/react-randomQuoteGen/static/js/2.14784463.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/react-randomQuoteGen/static/js/2.14784463.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2185ea97b74b3bff5d5",
+    "url": "/react-randomQuoteGen/static/js/main.5f992840.chunk.js"
+  },
+  {
+    "revision": "341ded1b5481c316418f",
+    "url": "/react-randomQuoteGen/static/js/runtime-main.fafa92ea.js"
+  }
+]);
